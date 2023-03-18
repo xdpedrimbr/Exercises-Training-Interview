e3a19a4a4238ba4f45d37bb3144c8bd6c3621091
Pedro Henrique Goncalves Teixeira <pedrohgt73@gmail.com>
2023-03-18 17:52:35 -0300

    Create twoSum.py

---
+++ NeetCode/twoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+solucao = Solution()
+print(solucao.twoSum([1, 2, 3, 4, 5], 9))
